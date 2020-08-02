9294c1ed7cbf72c0f49a7578631278c583a0955e
kingsley <ajaezokingsley@gmail.com>
2020-08-02 23:36:48 +0100

    changed the title of the app

---
+++ precache-manifest.bec40959a5e90a8306109c2b81746378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9014eaa61f497cd651698d68dd6d60f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d846781cec8bbba90083",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a13a4de7550757789594",
+    "url": "/static/js/2.e1b44fc5.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/static/js/2.e1b44fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d846781cec8bbba90083",
+    "url": "/static/js/main.11a79809.chunk.js"
+  },
+  {
+    "revision": "9e7999fd8f72b71008a4",
+    "url": "/static/js/runtime-main.09a58bb1.js"
+  }
+]);
